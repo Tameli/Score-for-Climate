58ff24b1bcb8371a6008e570243b693c9b68b3e4
kevinae <kevinaesch@gmail.com>
2020-05-02 15:35:16 +0200

    Main Activity and MainFragment implemented

--- app/src/main/java/com/example/scoreforclimate/MainFragment.kt
+++ app/src/main/java/com/example/scoreforclimate/MainFragment.kt
@@ -1,4 +1,24 @@
 package com.example.scoreforclimate
 
-class MainFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment(R.layout.fragment_main){
+
+    companion object {
+        fun newInstance(): MainFragment {
+            return MainFragment();
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonScorePoints.setOnClickListener { scoreSomePoints() }
+    }
+
+    private fun scoreSomePoints() {
+        parentFragmentManager.beginTransaction().replace(R.id.fragment_host, ScoreSomePointsFragment.newInstance()).addToBackStack("ScoreSomePoints").commit()
+    }
 }

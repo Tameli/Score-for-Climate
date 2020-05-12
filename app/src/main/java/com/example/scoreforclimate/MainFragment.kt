85acb92d112d709d4bf555e3bf30181e58bae437
kevinae <kevinaesch@gmail.com>
2020-05-12 17:36:59 +0200

    Small refactoring splitting getScoreFromDB into getScoreFromDB and setScoreOnDisplay to get current score without updating displayed score. Implemented bindService() and unbindService() to update foregroundService

--- app/src/main/java/com/example/scoreforclimate/MainFragment.kt
+++ app/src/main/java/com/example/scoreforclimate/MainFragment.kt
@@ -1,14 +1,16 @@
 package com.example.scoreforclimate
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import com.example.scoreforclimate.roomDB.Score
 import com.example.scoreforclimate.roomDB.ScoreDatabase
 import kotlinx.android.synthetic.main.fragment_main.*
-import java.text.DateFormat
 
 class MainFragment : Fragment(R.layout.fragment_main){
+
+    private var currentPointsConnection: CurrentPointsConnection? = null
 
     private val scoresDb by lazy {
         ScoreDatabase.getScoreDatabase(requireContext().applicationContext)
@@ -16,34 +18,72 @@ class MainFragment : Fragment(R.layout.fragment_main){
 
     companion object {
         fun newInstance(): MainFragment {
-            return MainFragment();
+            return MainFragment()
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bindService()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         buttonScorePoints.setOnClickListener { scoreSomePoints() }
-        buttonLoadScore.setOnClickListener { getScoreFromDB()}
+        buttonLoadScore.setOnClickListener { setScoreOnDisplay()}
         buttonSetPreconfig.setOnClickListener { openPreferences() }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unbindService()
+    }
+
+    //----------------------foreground service-----------------
+
+    private fun bindService() {
+        val demoService = Intent(context, CurrentPointService::class.java)
+        currentPointsConnection = CurrentPointsConnection()
+        currentPointsConnection?.let { it ->
+            requireActivity().bindService(demoService, it, Context.BIND_AUTO_CREATE)
+        }
+    }
+
+    private fun unbindService() {
+        requireActivity().unbindService(currentPointsConnection as CurrentPointsConnection)
+        currentPointsConnection = null
+    }
+
+    //--------------------button clicks----------------------
 
     private fun scoreSomePoints() {
         parentFragmentManager.beginTransaction().replace(R.id.fragment_host, ScoreSomePointsFragment.newInstance()).addToBackStack("ScoreSomePoints").commit()
     }
 
-    private fun getScoreFromDB() {
-
-        val scoreDao = scoresDb.scoreDao()
-        val score = scoreDao.getScoreById(1)
-        System.out.println(score.value)
-        if(score.value != null){
-            textViewCurrentScore.text = "You current score is" +" "+ score.value
-        }
-
+    private fun setScoreOnDisplay() {
+        textViewCurrentScore.text = "Your current score is ${getScoreFromDB()}"
     }
 
     private fun openPreferences() {
         parentFragmentManager.beginTransaction()
             .replace(R.id.fragment_host, ScorePreferenceFragment.newInstance()).addToBackStack("pref").commit()
+    }
+
+    //-------------------methods-----------------------------
+
+    private fun getScoreFromDB() :Int {
+        var points = -1
+        try {
+            val scoreDao = scoresDb.scoreDao()
+            val score = scoreDao.getScoreById(1)
+            println(score.value)
+            if (score.value != null) {
+                points = score.value!!
+                currentPointsConnection?.getCurrentPointsApi()?.showPoints(points)
+            }
+        } catch (e: NullPointerException) {
+            points = 0
+        }
+        return points
     }
 }
